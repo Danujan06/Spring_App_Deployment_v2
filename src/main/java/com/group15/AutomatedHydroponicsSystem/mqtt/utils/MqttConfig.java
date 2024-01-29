20a201e92cd417b5c4bd9b91e3e904f041f9ec0d
Danujan <sivasundaralingamdanujan@gmail.com>
2024-01-29 11:19:35 +0530

    Secrets hided

--- src/main/java/com/group15/AutomatedHydroponicsSystem/mqtt/utils/MqttConfig.java
+++ src/main/java/com/group15/AutomatedHydroponicsSystem/mqtt/utils/MqttConfig.java
@@ -1,3 +1,66 @@
+//package com.group15.AutomatedHydroponicsSystem.mqtt.utils;
+//
+//import com.amazonaws.services.iot.client.AWSIotException;
+//import com.amazonaws.services.iot.client.AWSIotMqttClient;
+//import com.amazonaws.services.iot.client.AWSIotQos;
+//import com.group15.AutomatedHydroponicsSystem.mqtt.dto.MyMessage;
+//import com.group15.AutomatedHydroponicsSystem.mqtt.dto.MyTopic;
+//import org.json.JSONObject;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class MqttConfig {
+//
+//    String clientEndpoint = "a398am5o3vbqfl-ats.iot.ap-south-1.amazonaws.com";
+//    String clientId = "Test_client";
+//    String awsAccessKeyId ="AKIATY6FSZD75ZSEJRU3";
+//    String awsSecretAccessKey = "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";
+//    AWSIotMqttClient client = null;
+//
+//    public String connectToIoT() throws AWSIotException, InterruptedException {
+//            client = new AWSIotMqttClient(clientEndpoint, clientId, awsAccessKeyId, awsSecretAccessKey, null);
+//            client.connect();
+//            System.out.println("Connected to IoT");
+//            return "Connected to IoT device successfully.";
+//    }
+//
+//    public void disconncetFromIoT() throws AWSIotException {
+//        client.disconnect();
+//        System.out.println("Disconnected from IoT");
+//    }
+//
+//
+//    public void publishData(Object payLoad, String topic) {
+//        AWSIotQos qos = AWSIotQos.QOS1;
+//        long timeout = 3000;
+//
+//        try {
+//            // Create a JSON object with your data
+//            JSONObject jsonPayload = new JSONObject();
+//            jsonPayload.put("message", payLoad);
+//
+//            // Convert the JSON object to a string
+//            String jsonString = jsonPayload.toString();
+//
+//            // Create a message with the JSON payload
+//            MyMessage message = new MyMessage(topic, qos, jsonString);
+//
+//            // Publish the message
+//            client.publish(message, timeout);
+//        } catch (AWSIotException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    public void subscribeToTopic(String topicName) throws AWSIotException {
+//        AWSIotQos qos = AWSIotQos.QOS1;
+//
+//        MyTopic topic = new MyTopic(topicName, qos);
+//        client.subscribe(topic);
+//        System.out.println("Hello, User You subscribed a topic");
+//    }
+//}
+
 package com.group15.AutomatedHydroponicsSystem.mqtt.utils;
 
 import com.amazonaws.services.iot.client.AWSIotException;
@@ -6,32 +69,31 @@ import com.amazonaws.services.iot.client.AWSIotQos;
 import com.group15.AutomatedHydroponicsSystem.mqtt.dto.MyMessage;
 import com.group15.AutomatedHydroponicsSystem.mqtt.dto.MyTopic;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MqttConfig {
 
-    String clientEndpoint = "a398am5o3vbqfl-ats.iot.ap-south-1.amazonaws.com";
-    String clientId = "Test_client";
-    String awsAccessKeyId ="AKIATY6FSZD75ZSEJRU3";
-    String awsSecretAccessKey = "0SeYOuGep538aDHiBO0VYAs613FN/sPn21H222YO";
-    AWSIotMqttClient client = null;
+    // Replace these annotations with configuration file or environment variable access
+    private final String clientEndpoint = System.getenv("AWS_IOT_CLIENT_ENDPOINT");
+    private final String clientId = System.getenv("AWS_IOT_CLIENT_ID");
+    private final String awsAccessKeyId = System.getenv("AWS_IOT_ACCESS_KEY_ID");
+    private final String awsSecretAccessKey = System.getenv("AWS_IOT_SECRET_ACCESS_KEY");
+
+    private AWSIotMqttClient client = null;
 
     public String connectToIoT() throws AWSIotException, InterruptedException {
-            client = new AWSIotMqttClient(clientEndpoint, clientId, awsAccessKeyId, awsSecretAccessKey, null);
-            client.connect();
-            System.out.println("Connected to IoT");
-//            Thread.sleep(10000);
-//            client.disconnect();
-//        System.out.println("Disconnected from IoT");
-            return "Connected to IoT device successfully.";
+        client = new AWSIotMqttClient(clientEndpoint, clientId, awsAccessKeyId, awsSecretAccessKey, null);
+        client.connect();
+        System.out.println("Connected to IoT");
+        return "Connected to IoT device successfully.";
     }
 
-    public void disconncetFromIoT() throws AWSIotException {
+    public void disconnectFromIoT() throws AWSIotException {
         client.disconnect();
         System.out.println("Disconnected from IoT");
     }
-
 
     public void publishData(Object payLoad, String topic) {
         AWSIotQos qos = AWSIotQos.QOS1;
@@ -60,6 +122,7 @@ public class MqttConfig {
 
         MyTopic topic = new MyTopic(topicName, qos);
         client.subscribe(topic);
-        System.out.println("Hello, User You subscribed a topic");
+        System.out.println("Hello, User! You subscribed to a topic");
     }
 }
+
